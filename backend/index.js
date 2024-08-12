73c4eadf02ef155ffcfde07af9e465320b0de41e
Voodels <vighneshpotdar@gmail.com>
2024-08-12 08:35:37 +0530

    Making cors changes

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./config/database.js";
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"; 
 import userRoute from "./routes/userRoute.js";
 import tweetRoute from "./routes/tweetRoute.js";
 import cors from "cors";
 
-dotenv.config({
-    path:".env"
-})
+dotenv.config({ path: ".env" });
 databaseConnection();
-const app = express(); 
 
-// middlewares
-app.use(express.urlencoded({
-    extended:true
-}));
+const app = express();
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
     origin: "https://twitter-clone-eight-virid.vercel.app",
-}
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+};
 
-app.use(cors({
-    origin:"https://twitter-clone-eight-virid.vercel.app",
-    credentials:true
-}));
-
-// changes -----------------------
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://twitter-clone-eight-virid.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-// api
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/tweet", tweetRoute);
- 
 
-app.listen(process.env.PORT,() => {
-    console.log(`Server listen at port ${process.env.PORT}`);
-})
+// API routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/tweet", tweetRoute);
+
+// Start server
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening at port ${process.env.PORT}`);
+});
